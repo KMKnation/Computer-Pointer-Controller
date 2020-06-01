a71e8644d7c5f3633a11f1c5aff5c179aaa5bf37
Mayur Kanojiya <kanojiyamayur@gmail.com>
2020-06-01 21:17:34 +0530

    fix: updated requirements

--- src/model.py
+++ src/model.py
@@ -32,15 +32,15 @@ class Model_X:
         raise NotImplementedError
 
     def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
+        '''
+        Before feeding the data into the model for inference,
+        you might have to preprocess it. This function is where you can do that.
+        '''
         raise NotImplementedError
 
     def preprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
+        '''
+        Before feeding the output of this model to the next model,
+        you might have to preprocess the output. This function is where you can do that.
+        '''
         raise NotImplementedError
