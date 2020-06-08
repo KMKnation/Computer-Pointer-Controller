bde81ac4492512a2d5794ce568caa6762ce31ee3
Mayur Kanojiya <kanojiyamayur@gmail.com>
2020-06-08 20:39:21 +0530

    chore: Model downloader related commands added

---
+++ runme.sh
@@ -0,0 +1,5 @@
+mkdir "models"
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-adas-binary-0001 -o models/
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name head-pose-estimation-adas-0001 -o models/ --precisions FP16
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name landmarks-regression-retail-0009 -o models/ --precisions FP16
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name gaze-estimation-adas-0002 -o models/ --precisions FP16
