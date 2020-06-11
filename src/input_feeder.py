4929af95ebfbfffc7bbce3df988ce0f81fe2753b
mayur.kanojiya <kanojiyamayur@gmail.com>
2020-06-11 11:49:45 +0530

    fix: added ip-address based cam

--- src/input_feeder.py
+++ src/input_feeder.py
@@ -7,8 +7,10 @@ Sample usage:
         do_something(batch)
     feed.close()
 '''
+import urllib
+
 import cv2
-from numpy import ndarray
+import numpy as np
 
 class InputFeeder:
     def __init__(self, input_type, input_file=None):
@@ -20,12 +22,17 @@ class InputFeeder:
         self.input_type=input_type
         if input_type=='video' or input_type=='image':
             self.input_file=input_file
+        else:
+            self.input_file= 'ip address'
     
     def load_data(self):
         if self.input_type=='video':
             self.cap=cv2.VideoCapture(self.input_file)
         elif self.input_type=='cam':
             self.cap=cv2.VideoCapture(0)
+        elif self.input_type=='ip-cam':
+            # self.cap=cv2.VideoCapture(0)
+            print('Using ip cam instead of webcam')
         else:
             self.cap=cv2.imread(self.input_file)
 
@@ -34,9 +41,15 @@ class InputFeeder:
         Returns the next image from either a video file or webcam.
         If input_type is 'image', then it returns the same image.
         '''
+        global frame
         while True:
-            for _ in range(10):
-                _, frame=self.cap.read()
+            if self.input_type=='ip=cam':
+                img_arr = np.array(bytearray(urllib.request.urlopen(self.input_file).read()), dtype=np.uint8)
+                frame = cv2.imdecode(img_arr, -1)
+                # cv2.imshow('IPWebcam', img)
+            else:
+                for _ in range(10):
+                    _, frame=self.cap.read()
             yield frame
 
 
