8c399117b46c4490fe25eb11bdc26afd7886a487
mayur.kanojiya <kanojiyamayur@gmail.com>
2020-06-15 17:57:37 +0530

    chore: commands fix

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 mkdir "models"
-#python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-adas-binary-0001 -o models/
+python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-adas-binary-0001 -o models/
 python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-adas-0001 -o models/
 python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name head-pose-estimation-adas-0001 -o models/
 python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name landmarks-regression-retail-0009 -o models/
