37ef67e169ea6d29915bb6f0318931daf21e3ee6
mayur.kanojiya <kanojiyamayur@gmail.com>
2020-06-16 15:17:16 +0530

    feat: added vtune script

--- runme_vtune.sh
+++ runme_vtune.sh
@@ -1,2 +1,3 @@
 source venv/bin/activate
-python model.py
+source /home/hb/intel/openvino/openvino/bin/setupvars.sh
+python src/driver.py -f models/intel/face-detection-adas-binary-0001/INT1/face-detection-adas-binary-0001.xml -l models/intel/landmarks-regression-retail-0009/FP16/landmarks-regression-retail-0009.xml -hp models/intel/head-pose-estimation-adas-0001/FP16/head-pose-estimation-adas-0001.xml -ge models/intel/gaze-estimation-adas-0002/FP16/gaze-estimation-adas-0002.xml -i bin/demo.mp4 -it video -d CPU -debug headpose gaze
